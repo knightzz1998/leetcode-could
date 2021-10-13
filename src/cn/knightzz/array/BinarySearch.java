d2e188bffeb8cf5c7d7a03b0102c0f0f95e82eb7
王天赐 <wangtianci2068@gmail.com>
2021-10-13 20:41:36 +0800

    Initial commit

---
+++ src/cn/knightzz/array/BinarySearch.java
@@ -0,0 +1,22 @@
+package cn.knightzz.array;
+
+public class BinarySearch {
+
+    class Solution {
+        public int search(int[] nums, int target) {
+
+            // 获取中间下标
+            int mid = nums.length / 2;
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = nums = new int[]{
+                -1, 0, 3, 5, 9, 12
+        };
+        int target = 9;
+        Solution solution = new BinarySearch().new Solution();
+        solution.search(nums, target);
+    }
+}
